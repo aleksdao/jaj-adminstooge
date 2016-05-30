f36cef66d5b1e017c9b8d64384b8d219ce199c03
Joel Darling <joel.darling@gmail.com>
2016-05-30 15:28:06 -0400

    queueStart and socket hookup

--- public/js/common/sequence.handler.js
+++ public/js/common/sequence.handler.js
@@ -6,20 +6,27 @@ app.factory('SequenceHandler', function(socket){
 
     loadSequence: function(sequence){
       _sequence = new Sequence(sequence);
+
+      //set Transport settings
       Tone.Transport.set(_sequence.getSettings());
+      Tone.Transport.scheduleRepeat(this.runEventHandler, _sequence.getSettings().resolution, 0);
     },
     getTransportState: function(){
       return Tone.Transport.state;
     },
-    queueStart: function(preRoll){
-
+    queueStart: function(preRoll, adjustForLatency){
+      adjustForLatency = adjustForLatency || true; //defaults to true
+      var adjustedStart = (preRoll - socket.getLatency()) / 1000;
+      Tone.Transport.start(adjustedStart); //start Transport
+    },
+    stop: function(){
+      Tone.Transport.position = 0;
+      return Tone.Transport.stop();
     }
-
   };
-
 });
 
-/////// Sequence prototype ///////
+/////// Sequence obj ///////
 function Sequence(sequence){
   if(!sequence)
     return;
