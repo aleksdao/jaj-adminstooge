d3d2213f80ed79778d80ea072403a310975e3845
Joel Darling <joel.darling@gmail.com>
2016-06-07 21:00:22 -0400

    updated readme

--- show_json_spec.js
+++ show_json_spec.js
@@ -1,20 +1,22 @@
-/// Show Spec v2 /// 
+/// Show Spec v2 ///
 
 {
-  id: String,
-  name: String,
-  length: String, // in 0:0:0 format
-  settings:
-  {
+  show_id: String,
+  show_name: String,
+  settings:{
     bpm: Number, //default 60
     time_sig: Number, //default 4
+    resolution: String //resolution to run tranport at. default 16n
   },
-  events:
-  { //hashtable of events with timestamps
-    "0:0:0": [
-        { action: 'actionName', params: {}, group: {} },
-        { action: 'actionName', params: {}, group: {} },
-      ],
-    "0:1:0": []//...etc
-  }
+  show_length: String, // in 0:0:0 format
+  events:[ //array of events with timestamps
+    {
+      time: "0:0:0",
+      action:"action_name",
+      params:{
+        //list of params for the action
+      },
+      group: String, //group ID.. defaults to all groups
+      preload: Boolean //queue for next cycle?
+  }]
 }
